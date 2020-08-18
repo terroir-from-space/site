7ea9d719242edf40a1732d2a6aeae335a55d5567
terroir-from-space <info@terroirfromspace.com>
2020-08-18 17:49:39 +0000

    Updates

---
+++ precache-manifest.b74454f2f9c3e772ce6308efad47fc08.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33c156bfc92d211857aa5de525e84af4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "609cf593ed7bb5138d20",
+    "url": "/static/css/main.bbd1b68f.chunk.css"
+  },
+  {
+    "revision": "803d12eab455b2a3202d",
+    "url": "/static/js/2.f45164e6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f45164e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "609cf593ed7bb5138d20",
+    "url": "/static/js/main.ad20aa8a.chunk.js"
+  },
+  {
+    "revision": "5430de67e0ee3864b707",
+    "url": "/static/js/runtime-main.0c1d3bfa.js"
+  },
+  {
+    "revision": "4295f865e56ce60f295757d52055b7eb",
+    "url": "/static/media/agrifood.4295f865.png"
+  },
+  {
+    "revision": "3b48841ef7665a9ccf41482e72d36e01",
+    "url": "/static/media/astropreneurs.3b48841e.svg"
+  },
+  {
+    "revision": "0dd5090c26dbc8e90b273e485f0c6a1b",
+    "url": "/static/media/email.0dd5090c.png"
+  },
+  {
+    "revision": "c701ce28d11162212c987c8fbfe47d52",
+    "url": "/static/media/parsec.c701ce28.png"
+  },
+  {
+    "revision": "1b66fae646e6c2806bc707be1c2ef9ae",
+    "url": "/static/media/space-1.1b66fae6.jpg"
+  },
+  {
+    "revision": "6525e13a439773e9e058b67ac21d2d6e",
+    "url": "/static/media/space-3.6525e13a.jpg"
+  },
+  {
+    "revision": "232be641ccfb364e0e7387c95ce4d9d8",
+    "url": "/static/media/startup3.232be641.png"
+  },
+  {
+    "revision": "086548a0c44e66392794cfc790583867",
+    "url": "/static/media/terroir-from-space-logo.086548a0.png"
+  },
+  {
+    "revision": "dd0f354236abdf9c805db5bd456ec6e6",
+    "url": "/static/media/vineyard-1.dd0f3542.jpg"
+  },
+  {
+    "revision": "a857601cfcc9bfc9afca9672ebf4f2a6",
+    "url": "/static/media/vineyard-2.a857601c.jpg"
+  },
+  {
+    "revision": "e64b999e7378049f4f3af1e26a75ea4a",
+    "url": "/static/media/vineyard-3.e64b999e.jpg"
+  },
+  {
+    "revision": "090daea33cd0d47dab065800d9e55334",
+    "url": "/static/media/youtube.090daea3.png"
+  }
+]);
